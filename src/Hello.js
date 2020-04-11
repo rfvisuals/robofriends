f1f305c3bb734d29c012106d1f83d25053afcdf6
worldpeer <contact@worldpeer.com>
2020-04-11 12:54:04 -0700

    created Hello.js component, installed tachyons

---
+++ src/Hello.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import './Hello.css'
+
+class Hello extends Component {
+    render() {
+        return (
+        <div className='f1 tac'>
+            <h1>Hello World</h1>
+            <p>Welcome to React</p>
+        </div>         
+  );
+ }
+}
+
+export default Hello;
