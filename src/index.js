7c686debc5d816fe3744ac1f7bc54ed9848b4d34
worldpeer <contact@worldpeer.com>
2020-04-11 15:19:26 -0700

    deleted native files, created Card component w/ props

--- src/index.js
+++ src/index.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Hello from './Hello';
-import * as serviceWorker from './serviceWorker';
+import Card from './Card';
+// import * as serviceWorker from './serviceWorker';
 import 'tachyons';
+import { robots } from './robots'; //destructure if export not default
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Hello />
-  </React.StrictMode>,
+  <div>
+    <Card id={robots[0].id} name={robots[0].name} email = {robots[0].email}/>
+    <Card id={robots[1].id} name={robots[1].name} email = {robots[1].email}/>
+    <Card id={robots[2].id} name={robots[2].name} email = {robots[2].email}/>
+    </div>,
   document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// serviceWorker.unregister();
