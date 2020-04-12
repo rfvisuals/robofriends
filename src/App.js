a6c4e8c79a71fd4c79035e48ea2ec3e4f24f02da
worldpeer <contact@worldpeer.com>
2020-04-12 13:48:59 -0700

    fetch api for robots, loading screen if delay

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
-import {robots} from './robots';
+
 
 
 
@@ -9,28 +9,42 @@ class App extends React.Component {
   constructor() {
     super()
     this.state = {
-      robots: robots,
+      robots: [],
       searchfield: ''
     }
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response =>  response.json())
+    .then(users => {
+      this.setState({robots: users})
+    })
+    
   }
   // Use Arrow Functions for own methods, 'this'
   onSearchChange = (event) => {
     this.setState({ searchfield: event.target.value});
-    
   }
-
 
   render() {
     const filteredRobots = this.state.robots.filter(robot => {
       return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
     });
-    return(
-    <div className='tc'>
-      <h1>RoboFriends</h1>
-      <SearchBox searchChange={this.onSearchChange}/>
-      <CardList robots={filteredRobots}/>
-    </div>
-    );
+
+    //Loading Status
+    if(this.state.robots.length === 0) {
+      return <h1>LOADING</h1>
+    } else {
+      return(
+        <div className='tc'>
+          <h1>RoboFriends</h1>
+           {/*COMPONENTS */}
+          <SearchBox searchChange={this.onSearchChange}/>
+          <CardList robots={filteredRobots}/>
+        </div>
+        );
+    }
   }
 }
 
